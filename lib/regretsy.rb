3574f7d55997b36da97a41fd350257e9f27ca947
violetaria <terri.chu@outlook.com>
2015-10-22 15:24:45 -0400

    get route for search returning basic data, index erb created which shows lump of data returned, no formatting

--- lib/regretsy.rb
+++ lib/regretsy.rb
@@ -1,5 +1,25 @@
 require "regretsy/version"
+require "sinatra/base"
+require "etsy"
+
+require "pry"
 
 module Regretsy
-  # Your code goes here...
+  class Dapper < Sinatra::Base
+    include Etsy
+    set :logging, true
+    set :auth_key, "#{ENV["AUTH_KEY"]}"
+
+    get "/search" do
+      Etsy.api_key = "#{settings.auth_key}"
+      response = Etsy::Request.get('/listings/active',
+                                   :includes => ['Images', 'Shop'],
+                                   :keywords => 'purple')
+      erb :index, locals: { data: response.result }
+#      binding.pry
+    end
+
+    run! if app_file == $0
+  end
 end
+
